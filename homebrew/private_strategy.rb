4894a875f561af4913ef081490dec7e09d802405
Jacquie Kellner <bookwyrm@alum.mit.edu>
2023-04-25 16:35:49 -0400

    feat: simplifying private_strategy to check for variables, set curl download params, and confirm github access

--- homebrew/private_strategy.rb
+++ homebrew/private_strategy.rb
@@ -38,7 +38,7 @@
 # strategy is suitable for corporate use just like S3DownloadStrategy, because
 # it lets you use a private GitHub repository for internal distribution.  It
 # works with public one, but in that case simply use CurlDownloadStrategy.
-class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
+class GitHubPrivateRepositoryReleaseDownloadStrategy < CurlDownloadStrategy
   require "utils/formatter"
   require "utils/github"
 
@@ -49,21 +49,20 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   end
 
   def parse_url_pattern
-    unless match = url.match(%r{https://github.com/([^/]+)/([^/]+)/(\S+)})
-      raise CurlDownloadStrategyError, "Invalid url pattern for GitHub Repository."
+    puts "URL:" +String(@url)
+    url_pattern = %r{https://api.github.com/repos/([^/]+)/([^/]+)/releases/assets/(\S+)}
+    unless @url =~ url_pattern
+      raise CurlDownloadStrategyError, "Invalid url pattern for GitHub Release."
     end
 
-    _, @owner, @repo, @filepath = *match
+    _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
   end
 
-  def download_url
-    "https://#{@github_token}@github.com/#{@owner}/#{@repo}/#{@filepath}"
-  end
 
   private
-
-  def _fetch(url:, resolved_url:, timeout:)
-    curl_download download_url, to: temporary_path
+  def _curl_args
+    args = ["-H", "Accept: application/octet-stream", "-u" "#{@github_token}:x-oauth-basic"]
+    args
   end
 
   def set_github_token
@@ -89,52 +88,3 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   end
 end
 
-# GitHubPrivateRepositoryReleaseDownloadStrategy downloads tarballs from GitHub
-# Release assets. To use it, add
-# `:using => GitHubPrivateRepositoryReleaseDownloadStrategy` to the URL section of
-# your formula. This download strategy uses GitHub access tokens (in the
-# environment variables HOMEBREW_GITHUB_API_TOKEN) to sign the request.
-class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDownloadStrategy
-  def initialize(url, name, version, **meta)
-    super
-  end
-
-  def parse_url_pattern
-    url_pattern = %r{https://github.com/([^/]+)/([^/]+)/releases/download/([^/]+)/(\S+)}
-    unless @url =~ url_pattern
-      raise CurlDownloadStrategyError, "Invalid url pattern for GitHub Release."
-    end
-
-    _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
-  end
-
-  def download_url
-    "https://#{@github_token}@api.github.com/repos/#{@owner}/#{@repo}/releases/assets/#{asset_id}"
-  end
-
-  private
-
-  def _fetch(url:, resolved_url:, timeout:)
-    # HTTP request header `Accept: application/octet-stream` is required.
-    # Without this, the GitHub API will respond with metadata, not binary.
-    curl_download download_url, "--header", "Accept: application/octet-stream", to: temporary_path
-  end
-
-  def asset_id
-    @asset_id ||= resolve_asset_id
-  end
-
-  def resolve_asset_id
-    release_metadata = fetch_release_metadata
-    assets = release_metadata["assets"].select { |a| a["name"] == @filename }
-    raise CurlDownloadStrategyError, "Asset file not found." if assets.empty?
-
-    assets.first["id"]
-  end
-
-  def fetch_release_metadata
-    #release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
-    #GitHub::API.open_rest(release_url)
-    GitHub.get_release(@owner, @repo, @tag)
-  end
-end
